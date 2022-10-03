03d1e8db1616a105bf46268175792bdd13c575e9
Pierre-M <meriaupierre.pro@gmail.com>
2022-10-03 09:56:14 +0200

    setup layouts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import "./core/theme/style.css";
 
+import { setupLayouts } from "virtual:generated-layouts";
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 
-import routes from "~pages";
+import generatedRoutes from "~pages";
+
+const routes = setupLayouts(generatedRoutes);
 
 import App from "./app.vue";
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,18 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import Pages from "vite-plugin-pages";
+import Layouts from "vite-plugin-vue-layouts";
 
 export default defineConfig({
-  plugins: [vue(), Pages()],
+  plugins: [
+    vue(),
+    Pages({
+      exclude: ["**/_layouts/**"],
+    }),
+    Layouts({
+      layoutsDirs: "./src/pages/_layouts",
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
